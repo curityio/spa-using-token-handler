ed9b80e4f42f4b1fbd242150ebd6f23d5e9fbcd4
sathyaduraisamy <sathya.duraisamy@curity.io>
2021-12-08 08:54:33 +0100

    Added Cypress framework for testing SPA

---
+++ spa/cypress/integration/spa.spec.js
@@ -0,0 +1,30 @@
+import {
+  BASE_URL,
+  TIMEOUT_100,
+  authenticateUser,
+  signOutUser,
+  clickElement } from './spa';
+
+describe('Single Page App Tests', () => {
+  beforeEach(() => {
+    cy.visit(BASE_URL);
+    clickElement('#startAuthentication');
+    authenticateUser();
+  });
+
+  afterEach(() => {
+    signOutUser();
+  });
+
+  it('Get user info and call APIs from the application', () => {
+    clickElement('#getUserInfo');
+    cy.get('#getUserInfoResult')
+        .wait(TIMEOUT_100)
+        .contains('Demo User');
+    clickElement('#getApiData');
+    cy.get('#getDataResult')
+        .wait(TIMEOUT_100)
+        .contains('Success response from the Business API');
+  })
+
+})
