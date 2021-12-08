e9b20e216b55d4e9a2ae5589d64f518c2445fb94
sathyaduraisamy <sathya.duraisamy@curity.io>
2021-12-08 08:54:33 +0100

    Added Cypress framework for testing SPA

---
+++ spa/cypress/integration/spa.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Curity AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const TIMEOUT_100 = 100;
+export const TIMEOUT_500 = 500;
+export const TIMEOUT_1000 = 1000;
+export const BASE_URL = Cypress.env('BASE_URL') || 'http://www.example.com/';
+export const USERNAME = 'demouser'
+export const PASSWORD = 'Password1'
+
+export function authenticateUser() {
+    cy.get('form').within(($form) => {
+        inputText('input[name=userName]', USERNAME)
+        inputText('input[name=password]', PASSWORD)
+        cy.root().submit()
+    })
+    cy.url().should('eq', BASE_URL);
+    cy.find('#getUserInfo')
+        .should('exist')
+}
+
+export function signOutUser() {
+    clickElement('#signOut');
+    cy.find('#startAuthentication')
+        .should('exist');
+}
+
+export function inputText(selector, text) {
+    cy.get(selector)
+        .click({ force: true })
+        .clear()
+        .type(text);
+}
+
+export function clickElement(selector, clickOptions) {
+    cy.get(selector)
+        .should('exist')
+        .click(clickOptions)
+        .wait(TIMEOUT_1000);
+}
