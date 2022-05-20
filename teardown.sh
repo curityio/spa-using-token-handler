b06cf36017828dac7a30425bab0cbc286820b313
gary.archer <gary.archer@WORKMAC.local>
2022-05-20 19:55:20 +0200

    Support different OAuth proxy implementations

--- teardown.sh
+++ teardown.sh
@@ -4,12 +4,6 @@
 # A script to free Docker resources when finished with development
 ##################################################################
 
-if [ "$1" == 'financial' ]; then
-  DEPLOYMENT_SCENARIO='financial'
-else
-  DEPLOYMENT_SCENARIO='standard'
-fi
-
 #
 # Ensure that we are in the folder containing this script
 #
@@ -18,5 +12,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 #
 # Free resources by running the child script
 #
-cd "./resources/$DEPLOYMENT_SCENARIO"
-./teardown.sh
+./resources/teardown.sh
