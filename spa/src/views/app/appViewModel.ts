0cf78e1e3634de6de98ef4a361819c24e209c130
gary.archer <gary.archer@WORKMAC.local>
2022-06-15 13:01:49 +0200

    View improvements

--- spa/src/views/app/appViewModel.ts
+++ spa/src/views/app/appViewModel.ts
@@ -1,29 +1,31 @@
 import {ApiClient} from '../../api/apiClient';
 import {Configuration} from '../../configuration';
 import {OAuthClient} from '../../oauth/oauthClient';
-import {StorageHelper} from '../../utilities/storageHelper';
 
 export class AppViewModel {
 
     public configuration: Configuration | null;
     public oauthClient: OAuthClient | null;
     public apiClient: ApiClient | null;
-    public storage: StorageHelper | null;
+    public initialized: boolean
 
     public constructor() {
         this.configuration = null;
         this.oauthClient = null;
         this.apiClient = null;
-        this.storage = null;
+        this.initialized = false;
     }
 
-    public async initialize(storage: StorageHelper): Promise<void> {
+    public async initialize(): Promise<void> {
 
-        const response = await fetch('config.json');
-        this.configuration = await response.json();
+        if (!this.initialized) {
 
-        this.oauthClient = new OAuthClient(this.configuration!.oauth);
-        this.apiClient = new ApiClient(this.configuration!.businessApiBaseUrl, this.oauthClient);
-        this.storage = storage;
+            const response = await fetch('config.json');
+            this.configuration = await response.json() as Configuration;
+
+            this.oauthClient = new OAuthClient(this.configuration.oauth);
+            this.apiClient = new ApiClient(this.configuration.businessApiBaseUrl, this.oauthClient);
+            this.initialized = true;
+        }
     }
 }
