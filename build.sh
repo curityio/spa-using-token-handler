4a5ee3f85403c51f1e5afccca66ad6d6d70352ec
gary.archer <gary.archer@WORKMAC.local>
2024-05-23 17:59:01 +0200

    Updates

--- build.sh
+++ build.sh
@@ -101,6 +101,13 @@ if [ $? -ne 0 ]; then
 fi
 
 #
+# TODO: delete after merge
+#
+cd resources
+git checkout feature/dependency_updates
+cd ..
+
+#
 # Build resources by running the child script
 #
 ./resources/build.sh $OAUTH_AGENT $OAUTH_PROXY
